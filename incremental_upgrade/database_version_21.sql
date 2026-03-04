108930a8843c4f2c415f685e57c0a1772830d63c
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-03-04 03:47:26 -0600

    Fix quotes in SQL update statements
    Corrected quotes in SQL update statements for stock URL and currency format.

--- incremental_upgrade/database_version_21.sql
+++ incremental_upgrade/database_version_21.sql
@@ -2,7 +2,7 @@
 REINDEX;
 
 -- Yahoo stock URL correction #7736
-UPDATE INFOTABLE_V1 SET INFOVALUE="https://finance.yahoo.com/quote/%s" WHERE INFONAME="STOCKURL" AND INFOVALUE="http://finance.yahoo.com/echarts?s=%s";
+UPDATE INFOTABLE_V1 SET INFOVALUE='https://finance.yahoo.com/quote/%s' WHERE INFONAME='STOCKURL' AND INFOVALUE='http://finance.yahoo.com/echarts?s=%s';
 
 -- Fix currency format for Hungarian Forint #6128
-UPDATE CURRENCYFORMATS_V1 SET SFX_SYMBOL=PFX_SYMBOL, PFX_SYMBOL=""  WHERE CURRENCY_SYMBOL="HUF" AND SFX_SYMBOL="";
+UPDATE CURRENCYFORMATS_V1 SET SFX_SYMBOL=PFX_SYMBOL, PFX_SYMBOL=''  WHERE CURRENCY_SYMBOL='HUF' AND SFX_SYMBOL='';
